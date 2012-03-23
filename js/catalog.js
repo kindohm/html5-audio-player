324864a1e567c857a3cf2066c6ab0f9567f0fe4e
Mike Hodnick <mike.hodnick@gmail.com>
2012-03-22 23:25:53 -0500

    changed to external audio

--- js/catalog.js
+++ js/catalog.js
@@ -4,20 +4,20 @@ var CATALOG = (function () {
 		songs : [
 			{ 
 				'id' : 1,
-				'title' : 'Brush',
-				'basePath' : 'audio/song1',
+				'title' : 'Elegance',
+				'basePath' : 'http://kindohm.com/music/Parser/RobotsStoleMyComputer/Parser.RobotsStoleMyComputer.01.Elegance', 
 				'formats' : [ 'ogg', 'mp3' ]
 			},
 			{
 				'id' : 2,
-				'title' : 'Snorkel',
-				'basePath' : 'audio/song2',
+				'title' : 'Less Human, More Mine',
+				'basePath' : 'http://kindohm.com/music/Parser/RobotsStoleMyComputer/Parser.RobotsStoleMyComputer.04.LessHumanMoreMine',
 				'formats' : [ 'ogg', 'mp3' ]
 			},
 			{
 				'id' : 3,
-				'title' : 'Stairs',
-				'basePath' : 'audio/song3',
+				'title' : 'Defragmentation : Fail',
+				'basePath' : 'http://kindohm.com/music/Parser/RobotsStoleMyComputer/Parser.RobotsStoleMyComputer.05.DefragmentationFail',
 				'formats' : [ 'ogg', 'mp3' ]
 			}
 		]
